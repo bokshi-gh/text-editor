7cbdf3885e21a4fdcdbcabc250681ac73f1da6b8
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-08-16 22:18:22 +0545

    Update main.c

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1) {
 	process_keypress(&e, c);
-        refresh_screen(&e);
+    refresh_screen(&e);
   }
 
   return 0;
