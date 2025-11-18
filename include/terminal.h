95b3b26e7b1a0fe99342a2dc4cb560529abb0d77
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-18 19:18:45 +0545

    add function declaration

--- include/terminal.h
+++ include/terminal.h
@@ -6,4 +6,17 @@ static struct termios orig;
 
 void enableRawMode();
 void disableRawMode();
-void get_terminal_width
+
+void get_terminal_dimentions();
+
+void move_cursor(unsigned int row, unsigned int column);
+void move_cursor_up();
+void move_cursor_down();
+void move_cursor_right();
+void move_cursor_left();
+void move_cursor_to_home();
+
+void clear_terminal();
+
+void switch_to_main_buffer();
+void switch_to_alternate_buffer();
