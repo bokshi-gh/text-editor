fc065121ed2f19bf49a79be8a5c23997f29707c6
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-18 19:33:43 +0545

    add script

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+rm -rf build
+mkdir -p build
+cd build
+
+echo -e "${GREEN}[GENERATING BUILD FILES]${NC}"
+cmake ..
+echo ""
+
+echo -e "${GREEN}[BUILDING THE PROJECT]${NC}"
+cmake --build .
+echo ""
+
+echo -e "${GREEN}[INSTALLING THE PROJECT]${NC}"
+cmake --install . --prefix ../dist
+
