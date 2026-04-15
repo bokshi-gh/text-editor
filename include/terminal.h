03e59ec2648f3c6378fde4bbfca91e1af17e4099
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-04-16 00:58:05 +0545

    add file

---
+++ include/terminal.h
@@ -0,0 +1,7 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+void switch_to_alternate_screen_buffer();
+void return_to_main_screen_buffer();
+
+#endif
---
+++ src/terminal.c
@@ -0,0 +1,5 @@
+#include "terminal.h"
+
+void switch_to_alternate_screen_buffer() write(STDOUT_FILENO, "\x1b[?1049h", 8);
+
+void return_to_main_screen_buffer() write(STDOUT_FILENO, "\x1b[?1049l", 8);
