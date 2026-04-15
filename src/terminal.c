295d14ad3bd0556fbcd0360516e080b150020fb9
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-04-16 01:33:09 +0545

    update

--- src/terminal.c
+++ src/terminal.c
@@ -10,7 +10,12 @@ void enable_raw_mode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
