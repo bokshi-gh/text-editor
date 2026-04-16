47cdefa3d4b0b79ba2f4217b95fcec9071c8ae31
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-04-16 08:04:38 +0545

    debug

--- include/editor.h
+++ include/editor.h
@@ -1,9 +1,15 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 #include "errors.h"
 
-void read_key();
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+char read_key();
 void process_keypress();
 
 #endif
--- src/editor.c
+++ src/editor.c
@@ -3,14 +3,17 @@
 char read_key() {
   int nread;
   char c;
+
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN) die("read");
   }
+
   return c;
 }
 
 void process_keypress() {
-  char c = editorReadKey();
+  char c = read_key();
+
   switch (c) {
     case CTRL_KEY('q'):
       exit(0);
